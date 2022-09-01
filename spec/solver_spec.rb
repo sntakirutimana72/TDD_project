3dbd22672799c1037b083ef574288ab2c21f419c
divine <divinemaina@gmail.com>
2022-09-01 15:07:39 +0200

    add fizzbuzz spec test with mocks

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -21,4 +21,23 @@ describe 'Solver' do
       expect(@solver_mock.reverse('amelia')).to eq('ailema')
     end
   end
+
+  context '#fizzbuzz' do
+    it 'should return string "fizz" if number is divisible by 3' do
+      allow(@solver_mock).to receive(:fizzbuzz).with(3).and_return('fizz')
+      expect(@solver_mock.fizzbuzz(3)).to eq 'fizz'
+    end
+  it 'should return string "Buzz" if number is divisible by 5' do
+    allow(@solver_mock).to receive(:fizzbuzz).with(5).and_return('buzz')
+    expect(@solver_mock.fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'should return string "FizzBuzz" if number is divisible by 3 and 5' do
+    allow(@solver_mock).to receive(:fizzbuzz).with(15).and_return('fizzbuzz')
+    expect(@solver_mock.fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'should return number as a string if number is not divisible by 3 or 5' do
+    allow(@solver_mock).to receive(:fizzbuzz).with(1).and_return('1')
+    expect(@solver_mock.fizzbuzz(1)).to eq '1'
+  end
+end
 end
